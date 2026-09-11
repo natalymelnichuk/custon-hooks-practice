37892341ebfcebd41b8cdbf4cdffafd989594d1c
Natallia <ntshmlnchuk@gmail.com>
2026-09-11 16:12:47 -0400

    UseDebounce hook file completed

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,1 +1,26 @@
+import { useState, useEffect } from "react";
 
+export interface DebounceInputs {
+    value: string;
+    delay: number;
+}
+
+export interface DebounceReturn {
+    debouncedValue: string;
+}
+
+export function useDebounce({ value, delay = 500 }: DebounceInputs): DebounceReturn {
+    const [debouncedValue, setDebouncedValue] = useState<string>(value);
+
+    useEffect(() => {
+        const handler = setTimeout(() => {
+            setDebouncedValue(value);
+        }, delay);
+        
+        return () => {
+            clearTimeout(handler);
+        };
+    }, [value, delay]);
+
+    return { debouncedValue };
+}
